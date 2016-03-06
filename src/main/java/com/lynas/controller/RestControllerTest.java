c21ed3ff94538d2dfabedf3336867b5f10fe5c13
LynAs <szlynas@gmail.com>
2016-03-06 12:05:21 +0600

    post done

--- src/main/java/com/lynas/controller/RestControllerTest.java
+++ src/main/java/com/lynas/controller/RestControllerTest.java
@@ -3,19 +3,25 @@ package com.lynas.controller;
 import com.lynas.appexception.Error;
 import com.lynas.appexception.PeopleNotFoundException;
 import com.lynas.model.People;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
 
 /**
  * Created by LynAs on 05-Mar-16
  */
 @RestController
 public class RestControllerTest {
-
+    public static final String APPLICATION_JSON = "application/json";
     @RequestMapping(value = "/people", method = RequestMethod.GET)
     public People people(){
-        throw new PeopleNotFoundException(1);
-        //return new People(1, "sazzad");
+        //throw new PeopleNotFoundException(1);
+        return new People(1, "sazzad");
     }
 
 
@@ -24,4 +30,20 @@ public class RestControllerTest {
     public Error peopleError(PeopleNotFoundException pe){
         return new Error(pe.getPeopleID(), "People of id not found");
     }
+
+
+
+    @RequestMapping(value = "/peoplePost", method = RequestMethod.POST, consumes = APPLICATION_JSON)
+    @ResponseBody
+    public ResponseEntity<People> peoplePost(@RequestBody People people, UriComponentsBuilder ucb){
+        HttpHeaders headers = new HttpHeaders();
+        URI locationUri = ucb
+                .path("people")
+                .path(String.valueOf(people.getId()))
+                .build()
+                .toUri();
+        headers.setLocation(locationUri);
+        return new ResponseEntity<>(people, headers, HttpStatus.CREATED);
+    }
+
 }
